9b0494d94fa9401ebcd3b8e442b07b3e39919ab2
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-02 12:47:08 -0300

    feat: Atualizando importações / Adicionando provider EmailIsUniqueValidator

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 
 import { UserRepository } from './user-repository/user-repository';
 import { UserController } from './user.controller';
+import { EmailIsUniqueValidator } from './validation/email-is-unique.validator';
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [UserRepository],
+  providers: [UserRepository, EmailIsUniqueValidator],
 })
 export class UserModule {}

3f1050460848ebc9ff0918cffb52e5464f7cf30c
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-02 12:18:16 -0300

    feat: Acrescentando método useGlobalPipes / Método responsável por adicionar pipes de validação e transformação (retornam exceções)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { useContainer } from 'class-validator';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /* GlobalPipes */
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  /* useGlobalPipes - Responsável por expressar o uso de pipes de validação(transform, whitelist e forbidNonWhitelisted). */
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
   await app.listen(3000);
 }
 bootstrap();

34c85a1aeda4788c77330cdf0672723cda96e125
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-07 23:05:58 -0300

    feat: Criação do método update / Método responsável por filtrar e atualizar usuários

--- src/user/user-repository/user-repository.ts
+++ src/user/user-repository/user-repository.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
 import { UserEntity } from '../user.entity';
 
-/* Classe Responsável por guardar dados dos novos usuários(users) e lista-los. */
+/* Classe Responsável por guardar, listar, editar e excluir os dados dos novos usuários(users). */
 
 @Injectable()
 
@@ -25,5 +26,23 @@ export class UserRepository {
     const possibleUser = this.users.find((usuario) => usuario.email === email);
 
     return possibleUser !== undefined;
+  }
+
+  async upadate(id: string, updateData: Partial<UserEntity>) {
+    const possibleUser = this.users.find((savedUser) => savedUser.id === id);
+
+    if (!possibleUser) {
+      throw new Error('Usuário não existe');
+    }
+
+    Object.entries(updateData).forEach(([key, value]) => {
+      if (key === 'id') {
+        return;
+      }
+
+      possibleUser[key] = value;
+    });
+
+    return possibleUser;
   }
 }

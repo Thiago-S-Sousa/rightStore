71fd2581efdb6f018ea3a50203df3e3ef5cbff5c
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-17 17:05:09 -0300

    feat: Adicionando Função searchById(Responsável por buscar usuário por id) e remove(Responsável por excluir usuário)

--- src/user/user-repository/user-repository.ts
+++ src/user/user-repository/user-repository.ts
@@ -16,11 +16,13 @@ export class UserRepository {
     this.users.push(user);
   }
 
-  /* Método save através do parâmetro user adiciona a users */
+  // Função save - Através do parâmetro user adiciona a users //
 
   async list() {
     return this.users;
   }
+
+  // ------ Função emailExists ------ //
 
   async emailExists(email: string) {
     const possibleUser = this.users.find((usuario) => usuario.email === email);
@@ -28,21 +30,43 @@ export class UserRepository {
     return possibleUser !== undefined;
   }
 
-  async upadate(id: string, updateData: Partial<UserEntity>) {
+  // Função responsável por verificar se um email já está cadastrado ou não //
+
+  // ------ Função searchById ------ //
+
+  private searchById(id: string) {
     const possibleUser = this.users.find((savedUser) => savedUser.id === id);
 
     if (!possibleUser) {
       throw new Error('Usuário não existe');
     }
 
+    return possibleUser;
+  }
+
+  // Função responsável por buscar usuário por id, usado nos Funções update e remove //
+
+  async upadate(id: string, updateData: Partial<UserEntity>) {
+    const user = this.searchById(id);
+
     Object.entries(updateData).forEach(([key, value]) => {
       if (key === 'id') {
         return;
       }
 
-      possibleUser[key] = value;
+      user[key] = value;
     });
 
-    return possibleUser;
+    return user;
+  }
+
+  // ------ Função remove ------ //
+
+  async remove(id: string) {
+    const user = this.searchById(id);
+
+    this.users = this.users.filter((savedUser) => savedUser.id !== id);
+
+    return user;
   }
 }

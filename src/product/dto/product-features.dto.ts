2248073201cb0ce72a5f066936bc749903a48316
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-17 21:35:25 -0300

    feat: ProductFeaturesDTO atualizado / Validações de atualização de produto definidas

--- src/product/dto/product-features.dto.ts
+++ src/product/dto/product-features.dto.ts
@@ -1,5 +1,12 @@
 /* eslint-disable prettier/prettier */
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class ProductFeaturesDTO {
+  @IsString()
+  @IsNotEmpty({ message: 'Nome da cadasterística não pode ser vazio' })
   name: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Descrição da característica não pode ser vazio' })
   description: string;
 }

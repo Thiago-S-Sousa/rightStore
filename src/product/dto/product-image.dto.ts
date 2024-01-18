74aaf197c2915d93bbf7902565431475cca42d0a
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-17 21:36:31 -0300

    feat: ProductImageDTO atualizado / Validações de imagem de produto definidas

--- src/product/dto/product-image.dto.ts
+++ src/product/dto/product-image.dto.ts
@@ -1,5 +1,13 @@
 /* eslint-disable prettier/prettier */
+import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
+
 export class ProductImageDTO {
+  // @IsUrl({ message: 'URL para imagem inválida' })
+
+  @IsUrl()
   url: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Descrição da imagem não pode ser vazia' })
   description: string;
 }

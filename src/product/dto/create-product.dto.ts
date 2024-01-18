5e8525c54f701164583f6f7ebb28801da876ee66
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-17 21:24:45 -0300

    feat: CreateProductDTO atualizado / Validações de criação de produto atualizadas/redefinidas

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,5 +1,16 @@
 /* eslint-disable prettier/prettier */
-import { IsNotEmpty, IsNumber, IsOptional, IsUUID } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  MaxLength,
+  Min,
+  ValidateNested,
+} from 'class-validator';
 
 import { ProductFeaturesDTO } from './product-features.dto';
 import { ProductImageDTO } from './product-image.dto';
@@ -8,26 +19,38 @@ export class CreateProductDTO {
   @IsUUID(undefined, { message: 'ID de usuário inválido' })
   userId: string;
 
-  @IsNotEmpty({ message: 'O nome não deve ser vazio' })
+  @IsString()
+  @IsNotEmpty({ message: 'Nome do produto não pode ser vazio' })
   name: string;
 
-  @IsNotEmpty({ message: 'O valor não deve ser vazio' })
-  @IsNumber()
+  @IsNumber({ maxDecimalPlaces: 2, allowNaN: false, allowInfinity: false })
+  @Min(1, { message: 'O valor precisa ser maior que zero' })
   value: number;
 
-  @IsOptional()
   @IsNumber()
+  @Min(0, { message: 'Quantidade mínima inválida' })
   amount: number;
 
-  @IsOptional()
+  @IsString()
+  @IsNotEmpty({ message: 'Descrição do produto não pode ser vazia ' })
+  @MaxLength(1000, {
+    message: 'Descrição não pode ter mais que 1000 caracteres',
+  })
   description: string;
 
-  @IsOptional()
-  features: ProductFeaturesDTO[];
+  @ValidateNested()
+  @IsArray()
+  // @ArrayMinSize(3)
+  @Type(() => ProductFeaturesDTO)
+  characteristics: ProductFeaturesDTO[];
 
-  @IsOptional()
+  @ValidateNested()
+  @IsArray()
+  @ArrayMinSize(1)
+  @Type(() => ProductImageDTO)
   images: ProductImageDTO[];
 
-  @IsNotEmpty({ message: 'A categoria não deve ser vazia' })
+  @IsString()
+  @IsNotEmpty({ message: 'Categoria do produto não pode ser vazia' })
   category: string;
 }

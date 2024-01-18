baa85924985a9fe42bd38c01c0b1023e927e1570
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-17 21:44:04 -0300

    feat: Função save, update e remove Incrementadas - Função listAll atualizada / Responsabilidades definidas para salvar, atualizar e remover produtos

--- src/product/product-repository/product-repository.ts
+++ src/product/product-repository/product-repository.ts
@@ -1,16 +1,47 @@
 /* eslint-disable prettier/prettier */
-
 import { Injectable } from '@nestjs/common';
+
+import { ProductEntity } from '../product.entity';
 
 @Injectable()
 export class ProductRepository {
-  private products = [];
+  private products: ProductEntity[] = [];
 
-  async save(product: any) {
-    this.products.push(product);
+  listAll() {
+    return this.products;
   }
 
-  async list() {
-    return this.products;
+  save(productData: ProductEntity) {
+    this.products.push(productData);
+    return productData;
+  }
+
+  private searchById(id: string) {
+    const possibleProduct = this.products.find((product) => product.id === id);
+
+    if (!possibleProduct) {
+      throw new Error('Produto não existe');
+    }
+
+    return possibleProduct;
+  }
+
+  async update(id: string, productData: Partial<ProductEntity>) {
+    const dataNotUpdatable = ['id', 'userId'];
+    const product = this.searchById(id);
+    Object.entries(productData).forEach(([key, value]) => {
+      if (dataNotUpdatable.includes(key)) {
+        return;
+      }
+      product[key] = value;
+    });
+
+    return product;
+  }
+
+  async remove(id: string) {
+    const productRemoved = this.searchById(id);
+    this.products = this.products.filter((product) => product.id !== id);
+    return productRemoved;
   }
 }

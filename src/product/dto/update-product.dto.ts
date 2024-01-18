ce55b6bb10a265600525b6bc7b0d4233004998b7
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-17 21:39:11 -0300

    feat: UpdateProductDTO incrementado / DTO responsável por definir validações de dados para atualização de produtos

---
+++ src/product/dto/update-product.dto.ts
@@ -0,0 +1,63 @@
+/* eslint-disable prettier/prettier */
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
+import { ProductFeaturesDTO } from './product-features.dto';
+import { ProductImageDTO } from './product-image.dto';
+
+export class UpdateProductDTO {
+  @IsUUID(undefined, { message: 'ID do produto inválido' })
+  id: string;
+
+  @IsUUID(undefined, { message: 'ID de usuário inválido' })
+  userId: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Nome do produto não pode ser vazio' })
+  @IsOptional()
+  name: string;
+
+  @IsNumber({ maxDecimalPlaces: 2, allowNaN: false, allowInfinity: false })
+  @IsOptional()
+  @Min(1, { message: 'O valor precisa ser maior que zero' })
+  @IsOptional()
+  value: number;
+
+  @IsNumber()
+  @Min(0, { message: 'Quantidade mínima inválida' })
+  @IsOptional()
+  availableQuantity: number;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @ValidateNested()
+  @IsArray()
+  // @ArrayMinSize(3)
+  @Type(() => ProductFeaturesDTO)
+  @IsOptional()
+  characteristics: ProductFeaturesDTO[];
+
+  @ValidateNested()
+  @IsArray()
+  @ArrayMinSize(1)
+  @Type(() => ProductImageDTO)
+  @IsOptional()
+  images: ProductImageDTO[];
+
+  @IsString()
+  @IsNotEmpty({ message: 'Categoria do produto não pode ser vazia' })
+  @IsOptional()
+  category: string;
+}

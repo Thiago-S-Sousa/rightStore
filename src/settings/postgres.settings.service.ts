649e7b044fa99c784ffa611eed97de88eb764bdd
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-20 10:29:54 -0300

    feat: Arquivo postgres.settings.service.ts criado - Datasource configurado / Arquivo responsável por fazer a conexão com o banco de dados(Postgres - TypeORM)

---
+++ src/settings/postgres.settings.service.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class PostgresSettingsService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_NAME'),
+      entities: [],
+      synchronize: true,
+    };
+  }
+}

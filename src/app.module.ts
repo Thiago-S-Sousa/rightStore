300f3d2e846736b9ab4fd30e3d722b1c1e253e61
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-20 10:33:35 -0300

    feat: Importações atializadas - ConfigModule, TypeOrmModule e PostgresSettingsService / Módulos instalados e configurados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProductModule } from './product/product.module';
+import { PostgresSettingsService } from './settings/postgres.settings.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [
+    UserModule,
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresSettingsService,
+      inject: [PostgresSettingsService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })

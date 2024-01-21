84ff3d88b9c689ddd2248824dff01374ad2089cf
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-21 20:19:55 -0300

    feat: Entidade de imagens de produto criada - ProductImageEntity / Responsável por definir a tabela(product_images) e as colunas correspondentes

---
+++ src/product/product-image.entity.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import { ProductEntity } from './product.entity';
+
+@Entity({ name: 'product_images' })
+export class ProductImageEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'url', length: 100, nullable: false })
+  url: string;
+
+  @Column({ name: 'description', length: 100, nullable: false })
+  description: string;
+
+  // RELACIONAMENTO N PRA 1 //
+
+  @ManyToOne(() => ProductEntity, (product) => product.images, {
+    orphanedRowAction: 'delete',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  product: ProductEntity;
+
+  // --------------------------- //
+}

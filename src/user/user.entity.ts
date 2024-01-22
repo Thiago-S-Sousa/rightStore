964a1bf32a818cdfae16aecdd5bf219bffc7a9cc
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-22 19:58:12 -0300

    feat: Entidade de usuário atualizada/configurada - Aplicando typeorm a UserEntity / Responsável por gerir a tabela(users) e as colunas correspondentes

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,7 +1,36 @@
 /* eslint-disable prettier/prettier */
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
 export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'name', length: 100, nullable: false })
   name: string;
+
+  @Column({ name: 'password', length: 70, nullable: false })
   password: string;
+
+  @Column({ name: 'email', length: 255, nullable: false })
   email: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 }
+
+// @Entity{name: 'users'}) - Faz referência a Tabela do Banco de Dados
+// @Column({name: 'name'...}) - Faz referência a coluna do Banco de Dados

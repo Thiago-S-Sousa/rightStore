74ec91a26b8b157f5818fd8e2326a3bc013e5787
Thiago Santos <Thiago_S_Sousa@outlook.com>
2024-01-21 22:46:22 -0300

    feat: Entidade de produto atualizada/configurada - Aplicando typeorm a ProductEntity / Responsável por gerir a tabela(products) e as colunas correspondentes

--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -1,22 +1,64 @@
 /* eslint-disable prettier/prettier */
-class ProductFeature {
-  name: string;
-  description: string;
-}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
-class ProductImage {
-  url: string;
-  description: string;
-}
+import { ProductCharacteristicEntity } from './product-characteristic.entity';
+import { ProductImageEntity } from './product-image.entity';
 
+@Entity({ name: 'products' })
 export class ProductEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'user_id', length: 100, nullable: false })
   userId: string;
+
+  @Column({ name: 'name', length: 100, nullable: false })
   name: string;
+
+  @Column({ name: 'value', nullable: false })
   value: number;
+
+  @Column({ name: 'amount', nullable: false })
   amount: number;
+
+  @Column({ name: 'description', length: 255, nullable: false })
   description: string;
+
+  @Column({ name: 'category', length: 100, nullable: false })
   category: string;
-  characteristics: ProductFeature[];
-  images: ProductImage[];
+
+  // RELACIONAMENTO 1 PRA MUITOS //
+
+  @OneToMany(
+    () => ProductCharacteristicEntity,
+    (productCharacteristicEntity) => productCharacteristicEntity.product,
+    { cascade: true, eager: true },
+  )
+  characteristics: ProductCharacteristicEntity[];
+
+  @OneToMany(
+    () => ProductImageEntity,
+    (productImageEntity) => productImageEntity.product,
+    { cascade: true, eager: true },
+  )
+  images: ProductImageEntity[];
+
+  // -------------------------------------- //
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 }
